60e557741fb840d077614ebaf2bffe8a788ab107
Urvi-patel27 <urvikathrotiyal@gmail.com>
2025-08-27 16:27:18 +0530

    p

---
+++ routes/employee.js
@@ -0,0 +1,13 @@
+import express from 'express'
+import authMiddleware from '../middleware/authMiddleware.js'
+import {addEmployee, upload,getEmployees,getEmployee,updateEmployee,fetchEmployeesByDepId} from '../controllers/employeeController.js'
+
+const router = express.Router()
+
+router.get('/', authMiddleware,getEmployees)
+router.post('/add', authMiddleware, upload.single('image'), addEmployee)
+router.get('/:id', authMiddleware,getEmployee)
+router.get('/department/:id', authMiddleware,fetchEmployeesByDepId)
+router.put('/:id', authMiddleware,updateEmployee)
+
+export default router
