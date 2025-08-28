1a401e63e53570c644d65fb7a43239476726cbdd
Urvi-patel27 <urvikathrotiyal@gmail.com>
2025-08-28 10:09:01 +0530

    url c

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ import leaveRouter from './routes/leave.js'
 import settingRouter from './routes/setting.js'
 import dashboardRouter from './routes/dashboard.js'
 import connectToDatabase from './db/db.js'
-// import {userRegister} from './userSeed.js'
+// import {userRegister} from './userSeed.js'/
 
 connectToDatabase()
 // userRegister()
@@ -32,3 +32,48 @@ app.use('/api/dashboard', dashboardRouter)
 app.listen(process.env.PORT, () => {
     console.log(`Server is Running on port ${process.env.PORT}`)
 })
+
+// ---------------server.js------------------------------------
+// import express from 'express';
+// import cors from 'cors';
+// import authRouter from './routes/auth.js';
+// import departmentRouter from './routes/department.js';
+// import employeeRouter from './routes/employee.js';
+// import salaryRouter from './routes/salary.js';
+// import leaveRouter from './routes/leave.js';
+// import settingRouter from './routes/setting.js';
+// import dashboardRouter from './routes/dashboard.js';
+// import connectToDatabase from './db/db.js';
+// import userRegister from './userSeed.js';
+
+
+// const app = express();
+
+
+// connectToDatabase();
+
+
+// userRegister();
+
+
+// app.use(cors({
+//     origin: "https://employee-frontend-jade-iota.vercel.app",
+//     credentials: true
+// }));
+// app.use(express.json());
+// app.use(express.static('public/uploads'));
+
+
+// app.use('/api/auth', authRouter);
+// app.use('/api/department', departmentRouter);
+// app.use('/api/employee', employeeRouter);
+// app.use('/api/salary', salaryRouter);
+// app.use('/api/leave', leaveRouter);
+// app.use('/api/setting', settingRouter);
+// app.use('/api/dashboard', dashboardRouter);
+
+
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => {
+//     console.log(`Server is running on port ${PORT}`);
+// });
