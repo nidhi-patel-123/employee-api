eb04ac168412bb8d8ee42aeed7c41e91a6129995
Urvi-patel27 <urvikathrotiyal@gmail.com>
2025-08-29 15:20:59 +0530

    io

--- routes/employee.js
+++ routes/employee.js
@@ -1,15 +1,40 @@
-import express from 'express'
-import authMiddleware from '../middleware/authMiddleware.js'
-import {addEmployee, upload,getEmployees,getEmployee,updateEmployee,fetchEmployeesByDepId,deleteEmployee} from '../controllers/employeeController.js'
+// import express from 'express'
+// import authMiddleware from '../middleware/authMiddleware.js'
+// import {addEmployee, upload,getEmployees,getEmployee,updateEmployee,fetchEmployeesByDepId,deleteEmployee} from '../controllers/employeeController.js'
 
-const router = express.Router()
+// const router = express.Router()
 
-router.get('/', authMiddleware,getEmployees)
-router.post('/add', authMiddleware, upload.single('image'), addEmployee)
-router.get('/:id', authMiddleware,getEmployee)
-router.get('/department/:id', authMiddleware,fetchEmployeesByDepId)
-router.put('/:id', authMiddleware,updateEmployee)
-router.delete('/:id', authMiddleware, deleteEmployee) // ← delete route add किया
+// router.get('/', authMiddleware,getEmployees)
+// router.post('/add', authMiddleware, upload.single('image'), addEmployee)
+// router.get('/:id', authMiddleware,getEmployee)
+// router.get('/department/:id', authMiddleware,fetchEmployeesByDepId)
+// router.put('/:id', authMiddleware,updateEmployee)
+// router.delete('/:id', authMiddleware, deleteEmployee) // ← delete route add किया
 
 
-export default router
+// export default router
+
+
+import express from "express";
+import authMiddleware from "../middleware/authMiddleware.js";
+import {
+  addEmployee,
+  getEmployees,
+  getEmployee,
+  updateEmployee,
+  fetchEmployeesByDepId,
+  deleteEmployee,
+  upload,
+} from "../controllers/employeeController.js";
+
+const router = express.Router();
+
+// Routes
+router.get("/", authMiddleware, getEmployees);
+router.post("/add", authMiddleware, upload.single("image"), addEmployee);
+router.get("/:id", authMiddleware, getEmployee);
+router.put("/:id", authMiddleware, updateEmployee);
+router.get("/department/:id", authMiddleware, fetchEmployeesByDepId);
+router.delete("/:id", authMiddleware, deleteEmployee);
+
+export default router;
